43ff487668730a6758d7884eaa622e965b8c0e57
jolatechno <joseph.touzet@gmail.com>
2022-04-01 17:57:28 +0200

    new selection skeem, and prepared for new results

--- validation/random_selector/repartition.py
+++ validation/random_selector/repartition.py
@@ -50,7 +50,7 @@ tested method of selection :
 
 def selector_1(List, n):
 	random_selector = np.random.rand(len(List))
-	random_selector = np.log( - np.log(random_selector) / List)
+	random_selector = random_selector / List #-np.log(random_selector) / List
 
 	idx = np.argpartition(random_selector, n)
 	return idx[:n]
@@ -65,7 +65,7 @@ generating the random distribution :
 random_list = np.ndarray([])
 
 # adding uniform distributions
-for rate in [1, 2, 4]:
+for rate in [1, 1.5, 3]:
 	distribution = np.random.rand(n_graph) * rate
 	distribution.sort()
 	random_list = np.append(random_list, distribution)
@@ -91,34 +91,52 @@ random_list /= np.sum(random_list)
 plotting the distribution and occurences for different proportions of graph selected :
 """
 
-fig = plt.figure(figsize=(10, 6), constrained_layout=True)
-plt.rc('axes', titlesize=16)
-plt.rc('figure', titlesize=19) 
-plt.rc('legend', fontsize=12)
+fig = plt.figure(figsize=(12, 5), constrained_layout=True)
+plt.rc('axes', labelsize=18)
+plt.rc('xtick', labelsize=18)
+plt.rc('figure', titlesize=20) 
+plt.rc('legend', fontsize=14)
 ax = fig.add_subplot(1, 1, 1)
 
-# setting up the frame
-ax.set_ylabel("probability")
-ax.set_yticks([])
-ax.set_xticks([])
+
 
 # plotting occurences for different proportions of graph selected
-for proportion in [.5, .25, .05]:
+for proportion in [.2, .1, .01]:
 	n_select = int(len(random_list) * proportion)
 	ax.plot(smooth(generate(selector_1, random_list, n_select)), label=f"log(log()), { proportion * 100 }% selected", linewidth=2)
 	#ax1.plot(smooth(generate(selector_2, random_list)), label="histograms")
 
+
+
 # plotting the initial ditribution
 ax.plot(random_list, "r--", label="base distribution", linewidth=3)
+
+
 
 # adding separators for the different distributions
 ax.set_ylim(ax.get_ylim())
 ax.plot([3*n_graph, 3*n_graph], [-1, 1], "k--", linewidth=4)
 ax.plot([6*n_graph, 6*n_graph], [-1, 1], "k--", linewidth=4)
 
+
+
+# adding legend
+legend = fig.legend(bbox_to_anchor=(0.99, 0.90), loc='upper right', borderaxespad=0)
+legend.get_frame().set_alpha(None)
+legend.get_frame().set_facecolor((1, 1, 1, 1)) # R G B transparency
+
+
+
+# setting up the frame
+ax.set_ylabel("probability")
+ax.set_yticks([])
+
+_, x_max = ax.get_xlim()
+ax.set_xticks([3*n_graph/2, (3*n_graph + 6*n_graph)/2, (6*n_graph + x_max)/2], ["linear distributions", "exponential distributions", "inverse polynomial distribution"])
+
 # adding text to name different sections
 y_max = ax.get_ylim()[1]
-args = {
+"""args = {
 	"horizontalalignment" : 'center',
 	"verticalalignment" : 'center',
 	"bbox" : {
@@ -128,9 +146,9 @@ args = {
 }
 ax.text(1.5*n_graph, y_max*.75, "linear distributions", **args)
 ax.text(4.5*n_graph, y_max*.75, "exponential distributions", **args)
-ax.text(7.5*n_graph, y_max*.75, "inverse polynomial distribution", **args)
+ax.text(7.5*n_graph, y_max*.75, "inverse polynomial distribution", **args)"""
 
-fig.legend(bbox_to_anchor=(1, 0.95), loc='upper right', borderaxespad=0)
+
 
 # saving the figure
 fig.suptitle(f'random repartition and occurences')
